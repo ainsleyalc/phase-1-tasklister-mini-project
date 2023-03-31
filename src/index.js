46cff071de83dc0e89fc0b9cc7cfde2d5c44eff9
Ainsley Alceme <youngchill101@gmail.com>
2023-03-31 16:30:01 -0400

    submits

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
+const textBox = document.getElementById("new-task-description")
+const taskForm = document.getElementById("create-task-form")
+const ulList = document.getElementById("tasks")
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+
+taskForm.addEventListener("submit",(event) =>{
+  event.preventDefault()
+const newLi = document.createElement("li")
+ let buttonx = document.createElement("button")
+  newLi.innerText = textBox.value
+  ulList.appendChild(newLi)
+  taskForm.reset()
+ 
+buttonx.textContent = "X"
+newLi.appendChild(buttonx)
+buttonx.addEventListener("click",()=> {
+  newLi.remove()
+})
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 });
